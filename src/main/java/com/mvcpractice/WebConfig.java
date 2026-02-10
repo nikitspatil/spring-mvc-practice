8f2dd6db1ad30bb6294afc79b4b535ecedf53bc7
Nikit Patil <nikitspatil@gmail.com>
2026-02-11 00:19:43 +0530

    Initial Commit

---
+++ src/main/java/com/mvcpractice/WebConfig.java
@@ -0,0 +1,10 @@
+package com.mvcpractice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.mvcpractice")
+public class WebConfig {
+
+}
